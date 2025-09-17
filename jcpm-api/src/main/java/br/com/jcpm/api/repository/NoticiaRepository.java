094be97355b4fae087fc8038f7f73314f8fb51ef
saulinho422 <saulo.rodrigues.gl@gmail.com>
2025-09-17 19:39:51 -0300

    Primeiro commit: projeto JCPM com API Spring Boot

---
+++ jcpm-api/src/main/java/br/com/jcpm/api/repository/NoticiaRepository.java
@@ -0,0 +1,10 @@
+package br.com.jcpm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jcpm.model.Noticia;
+
+@Repository
+public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
+}
